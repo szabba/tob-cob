5aed6849cf92243a9a83a38a33ad94389a1b4ff3
Karol Marcjan <karol.marcjan@gmail.com>
2019-09-19 19:22:00 +0200

    Add grid-cell drawing code

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"os"
 
 	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 
 	"github.com/rs/zerolog/log"
@@ -25,6 +26,13 @@ func run() {
 		Bounds: pixel.R(0, 0, 800, 600),
 		VSync:  true,
 	}
+
+	grid := Grid{}
+	grid.Cell.Width = 50
+	grid.Cell.Heigth = 25
+	grid.Spacing.Dx = 5
+	grid.Spacing.Dy = 5
+
 	humanoidSprite, err := loadSprite("assets/humanoid.png")
 	if err != nil {
 		log.Error().Err(err).Msg("")
@@ -36,16 +44,15 @@ func run() {
 		log.Error().Err(err).Msg("")
 		return
 	}
-	grid := Grid{}
-	grid.Cell.Width = 50
-	grid.Cell.Heigth = 25
-	grid.Spacing.Dx = 5
-	grid.Spacing.Dy = 5
 
 	for !w.Closed() {
 		w.Update()
 		center := w.Bounds().Center()
 		w.Canvas().SetMatrix(pixel.IM.Scaled(center.Scaled(-1), 2))
+		grid.DrawCell(w, 0, 0, pixel.RGB(1, 1, 1))
+		grid.DrawCell(w, 0, 1, pixel.RGB(1, 1, 1))
+		grid.DrawCell(w, 1, 0, pixel.RGB(1, 1, 1))
+		grid.DrawCell(w, 1, 1, pixel.RGB(1, 1, 1))
 		humanoidSprite.Draw(w, grid.Matrix(1, 1))
 		humanoidSprite.Draw(w, grid.Matrix(0, 1))
 		humanoidSprite.Draw(w, grid.Matrix(1, 0))
@@ -83,4 +90,15 @@ func (grid Grid) Matrix(col, row int) pixel.Matrix {
 	dy := grid.Cell.Heigth*y + grid.Spacing.Dy*y
 	dr := pixel.V(dx, dy)
 	return pixel.IM.Moved(dr)
+}
+
+func (grid Grid) DrawCell(dst pixel.Target, col, row int, color pixel.RGBA) {
+	imd := imdraw.New(nil)
+	imd.Color = color
+	imd.SetMatrix(grid.Matrix(col, row))
+	wHalf, hHalf := float64(grid.Cell.Width)/2, float64(grid.Cell.Heigth)/2
+	diagHalf := pixel.V(wHalf, hHalf)
+	imd.Push(diagHalf.Scaled(-1), diagHalf)
+	imd.Rectangle(1)
+	imd.Draw(dst)
 }
