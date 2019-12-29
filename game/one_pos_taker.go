9d55fa88dcbf7a238f2fb9c59aed0fe48ef8e358
Karol Marcjan <karol.marcjan@gmail.com>
2019-12-29 17:11:01 +0100

    game: Add OnePosTaker

---
+++ game/one_pos_taker.go
@@ -0,0 +1,34 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package game
+
+// A OnePosTaker is a space taker that occupies at most one grid position at a time.
+type OnePosTaker struct {
+	pos Position
+}
+
+// LetOnto is part of the SpaceTaker interface.
+func (taker *OnePosTaker) LetOnto(pos Position) {
+	if taker.Placed() {
+		taker.pos.Free()
+	}
+	taker.pos = pos
+}
+
+// ForceOff is part of the SpaceTaker interface.
+func (taker *OnePosTaker) ForceOff(pos Position) {
+}
+
+// Placed says whether the space taker is taking a position.
+func (taker *OnePosTaker) Placed() bool {
+	return taker.pos != Position{}
+}
+
+// AtPoint is the point at which the taken position is.
+//
+// The zero value of the type is returned when the taker is not placed.
+// Do not use that to check if the taker is placed.
+// A taker might be taking the position at the zero point.
+func (taker *OnePosTaker) AtPoint() Point { return taker.pos.AtPoint() }
---
+++ game/one_pos_taker_test.go
@@ -0,0 +1,85 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+package game_test
+
+import (
+	"testing"
+
+	"github.com/szabba/assert"
+	"github.com/szabba/tob-cob/game"
+)
+
+func TestOnePosTakerStartsEmpty(t *testing.T) {
+	// given
+	// when
+	taker := game.OnePosTaker{}
+
+	// then
+	assert.That(!taker.Placed(), t.Errorf, "the taker is placed - it should not")
+	assert.That(
+		taker.AtPoint() == game.Point{},
+		t.Errorf, "reported at point %#v - want %#v", taker.AtPoint(), game.Point{})
+}
+
+func TestOnePosTakerCanTakePosition(t *testing.T) {
+	// given
+	space := game.NewSpace()
+	pos := space.At(game.P(2, 3))
+	pos.Create()
+
+	taker := game.OnePosTaker{}
+
+	// when
+	pos.Take(&taker)
+
+	// then
+	assert.That(taker.Placed(), t.Errorf, "the taker is not placed - it should")
+	assert.That(
+		taker.AtPoint() == pos.AtPoint(),
+		t.Fatalf, "reported at point %#v - want %#v", taker.AtPoint(), pos.AtPoint())
+}
+
+func TestOnePosTakerCanBeMovedToNewPosition(t *testing.T) {
+	// given
+	space := game.NewSpace()
+	src := space.At(game.P(2, 3))
+	src.Create()
+	dst := space.At(game.P(2, 4))
+	dst.Create()
+
+	taker := game.OnePosTaker{}
+	src.Take(&taker)
+
+	// when
+	dst.Take(&taker)
+
+	// then
+	assert.That(taker.Placed(), t.Errorf, "the taker is not placed - it should")
+	assert.That(
+		taker.Position() != nil,
+		t.Fatalf, "got nil pointer to position, want one to position at %#v", dst.AtPoint())
+	assert.That(
+		*taker.Position() == dst,
+		t.Fatalf, "got pointer to position at %#v, want one to position at %#v",
+		taker.Position().AtPoint(), dst.AtPoint())
+}
+
+func TestOnePosTakerLeavesOldPosition(t *testing.T) {
+	// given
+	space := game.NewSpace()
+	src := space.At(game.P(2, 3))
+	src.Create()
+	dst := space.At(game.P(2, 4))
+	dst.Create()
+
+	taker := game.OnePosTaker{}
+	src.Take(&taker)
+
+	// when
+	dst.Take(&taker)
+
+	// then
+	assert.That(!src.Taken(), t.Errorf, "the original position is still taken")
+}
