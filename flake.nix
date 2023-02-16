cc3f4dbd18515e07fff0f21eec8d63ed98d727b7
Karol Marcjan <karol.marcjan@gmail.com>
2023-02-17 00:19:47 +0100

    flake.{nix,lock}: Add

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "A Nix-flake-based Go development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            # Packages included in the environment
+            buildInputs = with pkgs; [
+              # Nix
+              nixfmt
+
+              # Git
+              gitMinimal
+
+              # Go
+              go
+              gotools
+              golangci-lint
+              gopls
+              go-outline
+              gopkgs
+
+              # Editors
+              vscode
+
+              # CI
+              drone-cli
+              # TODO: use earthly
+            ];
+
+            # Run when the shell is started up
+            shellHook = ''
+              ${pkgs.go}/bin/go version
+            '';
+          };
+        };
+      });
+}
